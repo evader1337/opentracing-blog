0403a177b32a8596855bf6c1ded5fec4fad952e4
Domen Kajdič <domen.kajdic@gmail.com>
2019-02-09 17:29:03 +0100

    Adding tracing for non jax-rs functions

--- gama/src/main/java/com/dk/tracingblog/gama/Database.java
+++ gama/src/main/java/com/dk/tracingblog/gama/Database.java
@@ -1,10 +1,13 @@
 package com.dk.tracingblog.gama;
 
+import org.eclipse.microprofile.opentracing.Traced;
+
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import java.util.HashMap;
 
 @ApplicationScoped
+@Traced(value = true, operationName = "testingChangedOperationName")
 public class Database {
     private HashMap<Integer, String> data;
 
